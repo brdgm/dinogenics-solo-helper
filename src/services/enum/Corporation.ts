c3607cbdc466263bb3274ebf769cafb2bf84266b
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-01-02 19:08:34 +0100

    corporations

---
+++ src/services/enum/Corporation.ts
@@ -0,0 +1,11 @@
+/**
+ * Corporation
+ */
+enum Corporation {
+  BIOTHESAURI = 'biothesauri',
+  BLUESEA_GENETICS_PLC = 'bluesea-genetics-plc',
+  DINO_LIBRE = 'dino-libre',
+  NTEK = 'ntek',
+  TRAPEZOHEDRON_INCORPORATED = 'trapezohedron-incorporated'
+}
+export default Corporation
