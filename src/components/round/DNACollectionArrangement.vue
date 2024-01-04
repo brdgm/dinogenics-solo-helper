c1ebdbeb6c9afa62bb29c0a9d42e51523ab54a75
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-01-04 14:49:29 +0100

    layout

--- src/components/round/DNACollectionArrangement.vue
+++ src/components/round/DNACollectionArrangement.vue
@@ -2,12 +2,12 @@
   <ModalDialog id="dnaCollectionArrangementModal" :size-xl="true" :fullscreen-lg-down="true" :scrollable="true"
       :title="t('dnaCollectionArrangement.title')">
     <template #body>
-      <img src="@/assets/dna-collection-arrangement.png" alt="" class="arrangement-image"/>
+      <img src="@/assets/dna-collection-arrangement.png" alt="" class="arrangement-image mb-2"/>
       <p v-html="t('dnaCollectionArrangement.setCollection')"></p>
       <p v-html="t('dnaCollectionArrangement.cardOrder')"></p>
       <p v-html="t('dnaCollectionArrangement.noDuplicates')"></p>
       <h6 class="fst-italic">{{t('dnaCollectionArrangement.example.title')}}</h6>
-      <img src="@/assets/dna-collection-arrangement-example.jpg" alt="" class="arrangement-image"/>
+      <img src="@/assets/dna-collection-arrangement-example.jpg" alt="" class="arrangement-image mb-2"/>
       <p class="fst-italic" v-html="t('dnaCollectionArrangement.example.description')"></p>
     </template>
   </ModalDialog>
