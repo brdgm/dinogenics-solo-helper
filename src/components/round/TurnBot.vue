b5618b7af0b6ad4baad723d1fe7c61b6c631c38a
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-01-08 18:26:41 +0100

    add location components

--- src/components/round/TurnBot.vue
+++ src/components/round/TurnBot.vue
@@ -1,8 +1,13 @@
 <template>
   <p class="mt-4">{{ cardDeck.currentCard.id }} {{ validLocations }}</p>
 
-  <button class="btn btn-primary btn-lg mt-4" @click="$emit('next')">
-    {{t('action.next')}}
+  <component :is="`location-${currentLocation}`" :location="currentLocation" :bot="bot" :navigationState="navigationState"/>
+
+  <button class="btn btn-success btn-lg mt-4" @click="executed()">
+    {{t('action.executed')}}
+  </button>
+  <button class="btn btn-danger btn-lg mt-4 ms-2" @click="notPossible()">
+    {{t('action.notPossible')}}
   </button>
 </template>
 
@@ -14,9 +19,41 @@ import NavigationState from '@/util/NavigationState'
 import isLocationAvailable from '@/util/isLocationAvailable'
 import { defineComponent } from 'vue'
 import { useI18n } from 'vue-i18n'
+import LocationAgency from './location/LocationAgency.vue'
+import LocationBoneyard from './location/LocationBoneyard.vue'
+import LocationCityCenter from './location/LocationCityCenter.vue'
+import LocationDinogenicsIom from './location/LocationDinogenicsIom.vue'
+import LocationFarm from './location/LocationFarm.vue'
+import LocationFerry from './location/LocationFerry.vue'
+import LocationIntelligenDesignsHq from './location/LocationIntelligenDesignsHq.vue'
+import LocationOutsource from './location/LocationOutsource.vue'
+import LocationRefinement from './location/LocationRefinement.vue'
+import LocationSiteA from './location/LocationSiteA.vue'
+import LocationSiteB from './location/LocationSiteB.vue'
+import LocationSiteD from './location/LocationSiteD.vue'
+import LocationSpecialAction from './location/LocationSpecialAction.vue'
+import LocationTimeShare from './location/LocationTimeShare.vue'
+import LocationUplink from './location/LocationUplink.vue'
 
 export default defineComponent({
   name: 'TurnBot',
+  components: {
+    LocationAgency,
+    LocationBoneyard,
+    LocationCityCenter,
+    LocationDinogenicsIom,
+    LocationFarm,
+    LocationFerry,
+    LocationIntelligenDesignsHq,
+    LocationRefinement,
+    LocationOutsource,
+    LocationSiteA,
+    LocationSiteB,
+    LocationSiteD,
+    LocationSpecialAction,
+    LocationTimeShare,
+    LocationUplink
+  },
   emits: ['next'],
   setup() {
     const { t } = useI18n()
@@ -38,12 +75,29 @@ export default defineComponent({
     }
   },
   computed: {
-    cardDeck() : CardDeck {
+    cardDeck(): CardDeck {
       return this.bot.cardDeck
     },
-    validLocations() : Location[] {
+    validLocations(): Location[] {
       return this.cardDeck.currentCard.locations
           .filter(item => isLocationAvailable(item, this.navigationState.modules))
+    },
+    currentLocation() : Location {
+      return this.validLocations[this.currentLocationIndex]
+    }
+  },
+  methods: {
+    executed() {
+      this.$emit('next')
+    },
+    notPossible() {
+      if (this.currentLocationIndex < this.validLocations.length - 1) {
+        this.currentLocationIndex++
+      }
+      else {
+        this.bot.cardDeck.draw()
+        this.currentLocationIndex = 0
+      }
     }
   }
 })
---
+++ src/components/round/location/LocationFerry.vue
@@ -0,0 +1,38 @@
+<template>
+  <h3>Ferry</h3>
+  <p>TBD</p>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { useI18n } from 'vue-i18n'
+import AppIcon from '../../structure/AppIcon.vue'
+import NavigationState from '@/util/NavigationState'
+import Bot from '@/services/Bot'
+import Location from '@/services/enum/Location'
+
+export default defineComponent({
+  name: 'LocationFerry',
+  setup() {
+    const { t } = useI18n()
+    return { t }
+  },
+  components: {
+    AppIcon
+  },
+  props: {
+    location: {
+      type: String as PropType<Location>,
+      required: true
+    },
+    bot: {
+      type: Bot,
+      required: true
+    },
+    navigationState: {
+      type: NavigationState,
+      required: true
+    }
+  }
+})
+</script>
