13803c08e9f257fe6ebca8e78199585dc29d551a
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-01-11 12:07:04 +0100

    filter breaking news by module/expansion

--- src/components/rules/BreakingNewsModal.vue
+++ src/components/rules/BreakingNewsModal.vue
@@ -16,6 +16,8 @@
 import { defineComponent } from 'vue'
 import { useI18n } from 'vue-i18n'
 import ModalDialog from 'brdgm-commons/src/components/structure/ModalDialog.vue'
+import { useStateStore } from '@/store/state'
+import Module from '@/services/enum/Module'
 
 export default defineComponent({
   name: 'BreakingNewsModal',
@@ -24,21 +26,37 @@ export default defineComponent({
   },
   setup() {
     const { t } = useI18n()
-    return { t }
+    const state = useStateStore()
+    return { t, state }
   },
-  data() {
-    return {
-      cards: [
+  computed: {
+    hasControlledChaosExpansion() : boolean {
+      return this.state.setup.modules.includes(Module.CONTROLLED_CHAOS)
+    },
+    hasChaosTheoryModule() : boolean {
+      return this.state.setup.modules.includes(Module.CONTROLLED_CHAOS_CHAOS_THEORY)
+    },
+    cards() : string[] {
+      const cards = [
         '26',
         '27',
-        '29',
-        'D1',
-        'C01',
-        'C02',
-        'C03',
-        'C04',
-        'C05'
+        '29'        
       ]
+      if (this.hasControlledChaosExpansion) {
+        cards.push(
+          'D1'
+        )
+      }
+      if (this.hasChaosTheoryModule) {
+        cards.push(
+          'C01',
+          'C02',
+          'C03',
+          'C04',
+          'C05'
+        )
+      }
+      return cards
     }
   }
 })
