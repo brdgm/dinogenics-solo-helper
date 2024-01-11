d864915790d5e83333efbf0331083f32c538c941
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-01-11 18:17:44 +0100

    fix/complete final scoring

--- src/components/scoring/FinalScoring.vue
+++ src/components/scoring/FinalScoring.vue
@@ -3,14 +3,112 @@
     <thead>
       <tr>
         <th scope="col">
-          <h3 id="headlineFinalScoring" class="mt-3">{{t('endOfGame.scoring.title')}}</h3>
+          <h4 id="headlineFinalScoring" class="mt-3">{{t('endOfGame.resources.title')}}</h4>
         </th>
-        <th scope="col" v-for="player in playerCount+botCount" :key="player">
-          <AppIcon type="corporation" :name="playerCorporations[player]" class="corporation"/>
+        <th scope="col" v-for="player in totalPlayerCount" :key="player">
+          <AppIcon type="corporation" :name="playerCorporations[player-1]" class="corporation"/>
         </th>
       </tr>
     </thead>
     <tbody>
+      <tr>
+        <th scope="row">
+          {{t('endOfGame.resources.credits')}}
+        </th>
+        <td v-for="player in playerCount" :key="player">
+          <input type="number" min="0" max="99" step="1" v-model="resourcesCredits[player-1]" @focus="inputSelectAll"/>
+        </td>
+        <td v-for="bot in botCount" :key="bot">
+          <input v-if="botScoreCredits" type="number" min="0" max="99" step="1" v-model="resourcesCredits[playerCount+bot-1]" @focus="inputSelectAll"/>
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">
+          {{t('endOfGame.resources.dnaCards')}}
+        </th>
+        <td v-for="player in playerCount" :key="player">
+          <input type="number" min="0" max="99" step="1" v-model="resourcesDNACards[player-1]" @focus="inputSelectAll"/>
+        </td>
+        <td v-for="bot in botCount" :key="bot">
+          <input v-if="botScoreDNACards" type="number" min="0" max="99" step="1" v-model="resourcesDNACards[playerCount+bot-1]" @focus="inputSelectAll"/>
+        </td>
+      </tr>
+      <tr v-if="botScoreExtraRemainingStuff">
+        <th scope="row">
+          {{t('endOfGame.resources.remainingStuff')}}
+        </th>
+        <td v-for="player in playerCount" :key="player"></td>
+        <td v-for="bot in botCount" :key="bot">
+          <input type="number" min="0" max="99" step="1" v-model="resourcesRemainingStuff[playerCount+bot-1]" @focus="inputSelectAll"/>
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">
+          {{t('endOfGame.resources.dinosaurSpecies')}}
+        </th>
+        <td v-for="player in playerCount" :key="player">
+          <input type="number" min="0" max="99" step="1" v-model="dinosaurSpecies[player-1]" @focus="inputSelectAll"/>
+        </td>
+        <td v-for="bot in botCount" :key="bot">
+          <input v-if="botScoreDinosaurVariety" type="number" min="0" max="99" step="1" v-model="dinosaurSpecies[playerCount+bot-1]" @focus="inputSelectAll"/>
+        </td>
+      </tr>
+      <tr v-if="botScoreExtraDinosaursCreated">
+        <th scope="row">
+          {{t('endOfGame.resources.dinosaurCreated')}}
+        </th>
+        <td v-for="player in playerCount" :key="player"></td>
+        <td v-for="bot in botCount" :key="bot">
+          <input type="number" min="0" max="99" step="1" v-model="dinosaurCreated[playerCount+bot-1]" @focus="inputSelectAll"/>
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">
+          {{t('endOfGame.resources.scandalTokens')}}
+        </th>
+        <td v-for="player in playerCount" :key="player">
+          <input type="number" min="0" max="99" step="1" v-model="scandalTokens[player-1]" @focus="inputSelectAll"/>
+        </td>
+        <td v-for="bot in botCount" :key="bot">
+          <input type="number" min="0" max="99" step="1" v-model="scandalTokens[playerCount+bot-1]" @focus="inputSelectAll"/>
+        </td>
+      </tr>
+      <tr v-if="hasControlledChaos">
+        <th scope="row">
+          {{t('endOfGame.resources.financialAdvisor')}}
+          <div class="d-inline-flex"><label><input type="radio" v-model="financialAdvisorPlayerIndex" :value="undefined"/>n/a</label></div>
+        </th>
+        <td v-for="player in playerCount" :key="player">
+          <input type="radio" v-model="financialAdvisorPlayerIndex" :value="player-1"/>
+        </td>
+        <td v-for="bot in botCount" :key="bot">
+          <input type="radio" v-model="financialAdvisorPlayerIndex" :value="playerCount+bot-1"/>
+        </td>
+      </tr>
+    </tbody>
+    <thead>
+      <tr>
+        <th scope="col">
+          <h4 id="headlineFinalScoring" class="mt-3">{{t('endOfGame.scoring.title')}}</h4>
+        </th>
+        <th scope="col" v-for="player in totalPlayerCount" :key="player">
+          <AppIcon type="corporation" :name="playerCorporations[player-1]" class="corporation"/>
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">
+          {{t('endOfGame.scoring.vpGameBoard')}}
+          <div class="description">{{t('endOfGame.scoring.vpGameBoardDescription')}}</div>
+        </th>
+        <td v-for="player in playerCount" :key="player">
+          <input type="number" min="0" max="999" step="1" v-model="vpGameBoard[player-1]" @focus="inputSelectAll"/>
+        </td>
+        <td v-for="bot in botCount" :key="bot">
+          <input type="number" min="0" max="999" step="1" v-model="vpGameBoard[playerCount+bot-1]" @focus="inputSelectAll"/>
+        </td>
+      </tr>
       <tr>
         <th scope="row">
           {{t('endOfGame.scoring.vpFacilities')}}
@@ -27,6 +125,7 @@
         <th scope="row">
           {{t('endOfGame.scoring.vpSpecialists')}}
           <div class="description">{{t('endOfGame.scoring.vpSpecialistsDescription')}}</div>
+          <div class="description text-danger" v-if="financialAdvisorPlayerIndex != undefined">{{t('endOfGame.scoring.vpSpecialistsIgnoreFinancialAdvisor')}}</div>
         </th>
         <td v-for="player in playerCount" :key="player">
           <input type="number" min="0" max="99" step="1" v-model="vpSpecialists[player-1]" @focus="inputSelectAll"/>
@@ -41,22 +140,34 @@
           <div class="description">{{t('endOfGame.scoring.vpDinosaurVarietyDescription')}}</div>
         </th>
         <td v-for="player in playerCount" :key="player">
-          <input type="number" min="0" max="99" step="3" v-model="vpDinosaurVariety[player-1]" @focus="inputSelectAll"/>
+          {{vpDinosaurVariety[player-1]}}
         </td>
         <td v-for="bot in botCount" :key="bot">
-          <input v-if="botScoreDinosaurVariety" type="number" min="0" max="99" step="3" v-model="vpDinosaurVariety[playerCount+bot-1]" @focus="inputSelectAll"/>
+          {{vpDinosaurVariety[playerCount+bot-1]}}
+        </td>
+      </tr>
+      <tr v-if="botScoreExtraDinosaursCreated">
+        <th scope="row">
+          {{t('endOfGame.scoring.vpDinosaurCreated')}}
+          <div class="description">{{t('endOfGame.scoring.vpDinosaurCreatedDescription')}}</div>
+        </th>
+        <td v-for="player in playerCount" :key="player"></td>
+        <td v-for="bot in botCount" :key="bot">
+          {{vpDinosaurCreated[playerCount+bot-1]}}
         </td>
       </tr>
       <tr>
         <th scope="row">
           {{t('endOfGame.scoring.vpCredits')}}
           <div class="description">{{t('endOfGame.scoring.vpCreditsDescription')}}</div>
+          <div class="description" v-if="hasFinancialAdvisorPlayer">{{t('endOfGame.scoring.vpCreditsFinancialAdvisorPlayer')}}</div>
+          <div class="description" v-if="hasFinancialAdvisorBot">{{t('endOfGame.scoring.vpCreditsFinancialAdvisorBot')}}</div>
         </th>
         <td v-for="player in playerCount" :key="player">
-          <input type="number" min="0" max="99" step="1" v-model="vpCredits[player-1]" @focus="inputSelectAll"/>
+          {{vpCredits[player-1]}}
         </td>
         <td v-for="bot in botCount" :key="bot">
-          <input v-if="botScoreCredits" type="number" min="0" max="99" step="1" v-model="vpCredits[playerCount+bot-1]" @focus="inputSelectAll"/>
+          {{vpCredits[playerCount+bot-1]}}
         </td>
       </tr>
       <tr>
@@ -65,10 +176,20 @@
           <div class="description">{{t('endOfGame.scoring.vpDNACardsDescription')}}</div>
         </th>
         <td v-for="player in playerCount" :key="player">
-          <input type="number" min="0" max="99" step="1" v-model="vpDNACards[player-1]" @focus="inputSelectAll"/>
+          {{vpDNACards[player-1]}}
         </td>
         <td v-for="bot in botCount" :key="bot">
-          <input v-if="botScoreDNACards" type="number" min="0" max="99" step="1" v-model="vpDNACards[playerCount+bot-1]" @focus="inputSelectAll"/>
+          {{vpDNACards[playerCount+bot-1]}}
+        </td>
+      </tr>
+      <tr v-if="botScoreExtraRemainingStuff">
+        <th scope="row">
+          {{t('endOfGame.scoring.vpRemainingStuff')}}
+          <div class="description">{{t('endOfGame.scoring.vpRemainingStuffDescription')}}</div>
+        </th>
+        <td v-for="player in playerCount" :key="player"></td>
+        <td v-for="bot in botCount" :key="bot">
+          {{vpRemainingStuff[playerCount+bot-1]}}
         </td>
       </tr>
       <tr>
@@ -77,20 +198,10 @@
           <div class="description">{{t('endOfGame.scoring.vpScandalsDescription')}}</div>
         </th>
         <td v-for="player in playerCount" :key="player">
-          <input type="number" min="-96" max="0" step="6" v-model="vpScandals[player-1]" @focus="inputSelectAll"/>
+          {{vpScandals[player-1]}}
         </td>
         <td v-for="bot in botCount" :key="bot">
-          <input type="number" min="-96" max="0" step="6" v-model="vpScandals[playerCount+bot-1]" @focus="inputSelectAll"/>
-        </td>
-      </tr>
-      <tr v-if="botScoreExtra">
-        <th scope="row">
-          {{t('endOfGame.scoring.vpExtra')}}
-          <div class="description">{{t('endOfGame.scoring.vpExtraDescription')}}</div>
-        </th>
-        <td v-for="player in playerCount" :key="player"></td>
-        <td v-for="bot in botCount" :key="bot">
-          <input type="number" min="0" max="99" step="1" v-model="vpExtra[playerCount+bot-1]" @focus="inputSelectAll"/>
+          {{vpScandals[playerCount+bot-1]}}
         </td>
       </tr>
       <tr class="total">
@@ -106,15 +217,6 @@
       </tr>
     </tbody>
   </table>
-
-  <div v-if="botScoreExtra" class="mt-3">
-    <p v-html="t('endOfGame.scoring.extraVP.title')"></p>
-    <ul>
-      <li v-if="botScoreExtraFinancialAdvisor" v-html="t('endOfGame.scoring.extraVP.financialAdvisor')"></li>
-      <li v-if="botScoreExtraRemainingStuff" v-html="t('endOfGame.scoring.extraVP.remainingStuff')"></li>
-      <li v-if="botScoreExtraDinosaursCreated" v-html="t('endOfGame.scoring.extraVP.dinosaursCreated')"></li>
-    </ul>
-  </div>
 
   <p class="mt-3" v-html="t('endOfGame.scoring.determineWinner')"></p>
 </template>
@@ -146,18 +248,24 @@ export default defineComponent({
   },
   data() {
     return {
+      resourcesCredits: [] as number[],
+      resourcesDNACards: [] as number[],
+      resourcesRemainingStuff: [] as number[],
+      dinosaurSpecies: [] as number[],
+      dinosaurCreated: [] as number[],
+      scandalTokens: [] as number[],
+      financialAdvisorPlayerIndex : undefined as number|undefined,
+      vpGameBoard: [] as number[],
       vpFacilities: [] as number[],
-      vpSpecialists: [] as number[],
-      vpDinosaurVariety: [] as number[],
-      vpCredits: [] as number[],
-      vpDNACards: [] as number[],
-      vpScandals: [] as number[],
-      vpExtra: [] as number[]
+      vpSpecialists: [] as number[]
     }
   },
   computed: {
     hasControlledChaos() : boolean {
       return this.navigationState.hasControlledChaos
+    },
+    totalPlayerCount() : number {
+      return this.playerCount + this.botCount
     },
     difficultyLevel() : DifficultyLevel {
       return this.state.setup.difficultyLevel
@@ -177,9 +285,6 @@ export default defineComponent({
     botScoreDNACards() : boolean {
       return this.difficultyLevel != DifficultyLevel.VERY_EASY
     },
-    botScoreExtra() : boolean {
-      return this.botScoreExtraFinancialAdvisor || this.botScoreExtraRemainingStuff || this.botScoreExtraDinosaursCreated
-    },
     botScoreExtraFinancialAdvisor() : boolean {
       return this.hasControlledChaos &&
         (this.difficultyLevel == DifficultyLevel.NORMAL || this.difficultyLevel == DifficultyLevel.HARD || this.difficultyLevel == DifficultyLevel.VERY_HARD)
@@ -190,16 +295,103 @@ export default defineComponent({
     botScoreExtraDinosaursCreated() : boolean {
       return this.difficultyLevel == DifficultyLevel.VERY_HARD
     },
+    hasFinancialAdvisorPlayer() : boolean {
+      if (this.financialAdvisorPlayerIndex != undefined) {
+        return this.financialAdvisorPlayerIndex >= 0 && this.financialAdvisorPlayerIndex < this.playerCount
+      }
+      return false
+    },
+    hasFinancialAdvisorBot() : boolean {
+      if (this.financialAdvisorPlayerIndex != undefined) {
+        return this.financialAdvisorPlayerIndex >= this.playerCount && this.financialAdvisorPlayerIndex < this.totalPlayerCount
+      }
+      return false
+    },
+    vpDinosaurVariety() : number[] {
+      const result : number[] = []
+      for (let i=0; i<this.totalPlayerCount; i++) {
+        result[i] = this.toNumber(this.dinosaurSpecies[i], i, this.botScoreDinosaurVariety) * 3
+      }
+      return result
+    },
+    vpDinosaurCreated() : number[] {
+      const result : number[] = []
+      for (let i=0; i<this.playerCount; i++) {
+        result[i] = 0
+      }
+      for (let i=0; i<this.botCount; i++) {
+        const index = this.playerCount + i
+        result[index] = this.toNumber(this.dinosaurCreated[index], index, this.botScoreExtraDinosaursCreated) * 2
+      }
+      return result
+    },
+    vpCredits() : number[] {
+      const result : number[] = []
+      for (let i=0; i<this.playerCount; i++) {
+        const credits = this.toNumber(this.resourcesCredits[i], i, this.botScoreCredits)
+        if (i == this.financialAdvisorPlayerIndex) {
+          result[i] = Math.floor(credits / 2)
+        }
+        else {
+          result[i] = Math.floor(credits / 3)
+        }
+      }
+      for (let i=0; i<this.botCount; i++) {
+        const index = this.playerCount + i
+        let credits = this.toNumber(this.resourcesCredits[index], index, this.botScoreCredits)
+        if (index == this.financialAdvisorPlayerIndex) {
+          if (credits >= 10) {
+            result[index] = 5
+            credits = credits - 10
+          }
+          else {
+            result[index] = 0
+          }
+          result[index] += Math.floor(credits / 3)
+        }
+        else {
+          result[index] = Math.floor(credits / 3)
+        }
+      }
+      return result
+    },
+    vpDNACards() : number[] {
+      const result : number[] = []
+      for (let i=0; i<this.totalPlayerCount; i++) {
+        result[i] = Math.floor(this.toNumber(this.resourcesDNACards[i], i, this.botScoreDNACards) / 2)
+      }
+      return result
+    },
+    vpRemainingStuff() : number[] {
+      const result : number[] = []
+      for (let i=0; i<this.playerCount; i++) {
+        result[i] = 0
+      }
+      for (let i=0; i<this.botCount; i++) {
+        const index = this.playerCount + i
+        result[index] = this.toNumber(this.resourcesRemainingStuff[index], index, this.botScoreExtraRemainingStuff)
+      }
+      return result
+    },
+    vpScandals() : number[] {
+      const result : number[] = []
+      for (let i=0; i<this.totalPlayerCount; i++) {
+        result[i] = this.toNumber(this.scandalTokens[i], i) * -6
+      }
+      return result
+    },
     vpTotal() : number[] {
       const result : number[] = []
-      for (let i=0; i<this.playerCount+this.botCount; i++) {
-        result[i] = this.toNumber(this.vpFacilities[i], i, this.botScoreFacilities)
+      for (let i=0; i<this.totalPlayerCount; i++) {
+        result[i] = this.toNumber(this.vpGameBoard[i], i)
+            + this.toNumber(this.vpFacilities[i], i, this.botScoreFacilities)
             + this.toNumber(this.vpSpecialists[i], i, this.botScoreSpecialists)
-            + this.toNumber(this.vpDinosaurVariety[i], i, this.botScoreDinosaurVariety)
-            + this.toNumber(this.vpCredits[i], i, this.botScoreCredits)
-            + this.toNumber(this.vpDNACards[i], i, this.botScoreDNACards)
-            + this.toNumber(this.vpScandals[i], i)
-            + this.toNumber(this.vpExtra[i], i, this.botScoreExtra)
+            + this.vpDinosaurVariety[i]
+            + this.vpDinosaurCreated[i]
+            + this.vpCredits[i]
+            + this.vpDNACards[i]
+            + this.vpRemainingStuff[i]
+            + this.vpScandals[i]
       }
       return result
     }
